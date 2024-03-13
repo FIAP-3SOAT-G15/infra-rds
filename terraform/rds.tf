e4a94dd2bc9fe731affafa5c359999571d31b818
Wellyson Freitas <wellyson.freitas@n26.com>
2024-03-13 22:37:36 +0100

    Use param group for Postgres 15.6

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "default" {
   engine                      = "postgres"
   engine_version              = "15.6"
   instance_class              = "db.t3.micro"
-  parameter_group_name        = "default.postgres16"
+  parameter_group_name        = "default.postgres15"
   skip_final_snapshot         = true
   username                    = "master"
   manage_master_user_password = true
