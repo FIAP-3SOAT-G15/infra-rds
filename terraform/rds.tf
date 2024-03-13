0052f279387751d0395ce2333dadb13b29ebe30e
Wellyson Freitas <wellyson.freitas@n26.com>
2024-03-13 22:46:33 +0100

    Change identifier

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,7 @@
 resource "aws_db_instance" "default" {
   allocated_storage           = 10
   db_name                     = "selfordermanagement"
+  identifier                  = "selfordermanagement"
   engine                      = "postgres"
   engine_version              = "15.6"
   instance_class              = "db.t3.micro"
