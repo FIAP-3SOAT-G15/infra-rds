5d8556d9024def99d76a295c510a42ff5149096f
Wellyson Freitas <wellyson.freitas@n26.com>
2024-03-16 08:25:16 +0100

    Move to us-east-1

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,16 +1,16 @@
-#resource "aws_db_instance" "default" {
-#  allocated_storage           = 10
-#  db_name                     = "selfordermanagement"
-#  identifier                  = "selfordermanagement"
-#  engine                      = "postgres"
-#  engine_version              = "15.6"
-#  instance_class              = "db.t3.micro"
-#  parameter_group_name        = "default.postgres15"
-#
-#  username                    = "master"
-#  manage_master_user_password = true
-#  
-#  multi_az                    = false
-#  backup_retention_period     = 0
-#  skip_final_snapshot         = true
-#}
+resource "aws_db_instance" "default" {
+  allocated_storage           = 10
+  db_name                     = "selfordermanagement"
+  identifier                  = "selfordermanagement"
+  engine                      = "postgres"
+  engine_version              = "15.6"
+  instance_class              = "db.t2.micro"
+  parameter_group_name        = "default.postgres15"
+
+  username                    = "master"
+  manage_master_user_password = true
+
+  multi_az                    = false
+  backup_retention_period     = 0
+  skip_final_snapshot         = true
+}
